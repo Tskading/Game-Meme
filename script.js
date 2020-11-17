780e2d616b5ba99792ddbc2ac5d7b0234dbc93bf
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-11-16 19:24:22 -0800

    developed slideshow utulizing GIPHY API

--- script.js
+++ script.js
@@ -1,49 +1,60 @@
 // GIPHY API
-var giphyURL = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=0p6VOTrg6ToWiukHOTdQlHu3z5d9TSee&limit=5";
 
-$.ajax({
-    url: giphyURL,
-    method: "GET"
-}).then((data) =>{
-console.log(data);
-carouselGenerator(data);
-});
+function gifSlideShow() {
 
-function carouselGenerator(gifData) {
-    console.log(gifData);
-    // Initialize all div with carousel class
-var carousels = bulmaCarousel.attach('.carousel', options);
+	var game = localStorage.getItem('game');
 
-// Loop on each carousel initialized
-for(var i = 0; i < carousels.length; i++) {
-	// Add listener to  event
-	carousels[i].on('before:show', state => {
-		console.log(state);
-	});
+	console.log(game);
+
+	for (i = 0; i < game.length; i++) { 
+	  var giphyURL = `http://api.giphy.com/v1/gifs/search?q=${game}&api_key=0p6VOTrg6ToWiukHOTdQlHu3z5d9TSee&limit=5`;
+	  
+	 console.log(giphyURL);
+ 
+		$.ajax({
+			url: giphyURL,
+			method: "GET"
+		}).then(function(response){
+			console.log(response);
+			$('#gif1').attr("src", response.data[0].images.downsized_medium.url); 
+			$('#gif2').attr("src", response.data[1].images.downsized_medium.url);
+			$('#gif3').attr("src", response.data[2].images.downsized_medium.url);
+			$('#gif4').attr("src", response.data[3].images.downsized_medium.url)
+			$('#gif5').attr("src", response.data[4].images.downsized_medium.url)
+		}
+		)}
 }
 
-// Access to bulmaCarousel instance of an element
-var element = document.querySelector('#my-element');
-if (element && element.bulmaCarousel) {
-	// bulmaCarousel instance is available as element.bulmaCarousel
-	element.bulmaCarousel.on('before-show', function(state) {
-		console.log(state);
-	});
+
+//GIF Slide Show
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-// giphyURL.done(function(data) { 
-//     console.log("success got giphy data", data); 
-// }).then(function(data) {
-//     console.log(data);
-
-
-
-//   $("#imageGame").text(data.data[0].images.downsized.url);
-  
-
-//   $("#giphyDIV").html("<img src= 'gifURL' />");
-// });
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // RAWG API
@@ -54,7 +65,10 @@ $(".button").on("click", function(e){
 
 	var game = $("#userGameInput").val().trim();
 
-	localStorage.setItem("game", game);
+	localStorage.setItem('game', game);
+	gifSlideShow();
+
+	console.log(game);
 
 	var queryURL = `https://api.rawg.io/api/games?search=${game}&key=e318c637851a4a5b9428f416408759cc`;
 
@@ -67,9 +81,9 @@ $(".button").on("click", function(e){
 		// console.log(response.results[0].backgroun_image);
 
 
-		$("#gameImage").attr("src", response.results[0].background_image);
+		$("#main-img").attr("src", response.results[0].background_image);
 		$(".card-header-title").text(response.results[0].name);
 		// $("#gameTextEl").text(response.results[0].);
 	});
-
+	
 });
