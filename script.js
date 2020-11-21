aa7f0b70a7eee4b6b0470e1f0ef68d2d2f01ee74
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-11-21 13:22:50 -0800

    added function to display last search on refresh, added a reset button in modal to clear last search

--- script.js
+++ script.js
@@ -1,3 +1,106 @@
+///////////////////////// Display Game Content //////////////////////////////////
+// Search function
+var gameSearch = $('#userGameInput');
+var search = [];
+
+$("#searchBtn").click( function searchButton(e){
+	e.preventDefault();
+	var submittedSearch = gameSearch.submit();
+	var userChoice = submittedSearch.val().toLowerCase();
+	search.push(userChoice);
+    console.log(search);
+
+	for (i = 0; i < search.length; i++) { 
+        var game = search[i]
+        localStorage.setItem('game', game);
+        console.log(game);
+
+	gifSlideShow();
+	displayGameInfo();
+	}
+});
+
+//Display game content with RAWG API
+function displayGameInfo() {
+
+	var game = localStorage.getItem('game');
+	var queryURL = `https://api.rawg.io/api/games?search=${game}&key=e318c637851a4a5b9428f416408759cc`;
+	
+	// localStorage.setItem("game", game);
+	
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+		}).then(function(response){
+			console.log(response);
+			// Set youtube game ID in local storage here 
+			localStorage.setItem("gameID", response.results[0].id);
+			// console.log(response.results[0].backgroun_image);
+
+			// Clears card Info
+			$("#gameReturns").empty();
+
+			// Sets image card image
+			$("#main-img").addClass("game-Image").attr("src", response.results[0].background_image);
+
+			// Sets title based on search
+			$(".card-header-title").text(response.results[0].name);
+
+			// Displays release date
+			var releaseDate = response.results[0].released;
+			var li = $("<li>");
+
+			li.addClass("release").text("Release Date: " + releaseDate);
+			$("#gameReturns").prepend(li);
+
+			// Displays metacritic score
+			var metaCritic = response.results[0].metacritic;
+			var li = $("<li>");
+
+			li.text("Metacritic Score: " + metaCritic);
+			$("#gameReturns").append(li);
+
+			// Displays platforms game is available on
+			var platforms = response.results[0].platforms;
+
+			console.log("platforms", platforms);
+
+			for ( var i = 0; i < platforms.length; i++) {
+				var li = $("<li>");
+
+				li.text(platforms[i].platform.name);
+				$("#gameReturns").append(li);
+			};
+			
+
+			// Calls youtube function to generate info for link in 
+			getYouTube();
+							
+	})
+}
+
+// This section handles the youtube video data
+function getYouTube(response2){
+	// response2.preventDefault();
+	
+	var id = localStorage.getItem("gameID");
+	var queryYouTube = `https://api.rawg.io/api/games/${id}/youtube`;
+	
+	
+	$.ajax({
+		url: queryYouTube,
+		method: "GET"
+	}).then(function(response2){
+		console.log(response2);
+		// console.log("id:", id);
+		// console.log(externalID);
+
+		var externalID = response2.results[0].external_id;
+		$("#game-title").attr("href", "https://www.youtube.com/watch?v=" + externalID);
+	});
+};
+
+///////////////////////// Display Slide Show //////////////////////////////////
 // GIPHY API
 
 function gifSlideShow() {
@@ -56,115 +159,6 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-// RAWG API
-var gameSearch = $('#userGameInput');
-
-$(".button").click( function searchButton(e){
-	e.preventDefault();
-	console.log(e);
-
-	var search = gameSearch.submit();
-	var game = search.val().trim();
-
-	localStorage.setItem('game', game);
-	gifSlideShow();
-
-	console.log(game);
-
-	var queryURL = `https://api.rawg.io/api/games?search=${game}&key=e318c637851a4a5b9428f416408759cc`;
-	
-	localStorage.setItem("game", game);
-	
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-		}).then(function(response){
-			console.log(response);
-			// Set youtube game ID in local storage here 
-			localStorage.setItem("gameID", response.results[0].id);
-			// console.log(response.results[0].backgroun_image);
-
-			// Clears card Info
-			$("#gameReturns").empty();
-
-			// Sets image card image
-			$("#main-img").addClass("game-Image").attr("src", response.results[0].background_image);
-
-			// Sets title based on search
-			$(".card-header-title").text(response.results[0].name);
-
-			// Displays release date
-			var releaseDate = response.results[0].released;
-			var li = $("<li>");
-
-			li.addClass("release").text("Release Date: " + releaseDate);
-			$("#gameReturns").prepend(li);
-
-			// Displays metacritic score
-			var metaCritic = response.results[0].metacritic;
-			var li = $("<li>");
-
-			li.text("Metacritic Score: " + metaCritic);
-			$("#gameReturns").append(li);
-
-			// Displays platforms game is available on
-			var platforms = response.results[0].platforms;
-
-			console.log("platforms", platforms);
-
-			for ( var i = 0; i < platforms.length; i++) {
-				var li = $("<li>");
-
-				li.text(platforms[i].platform.name);
-				$("#gameReturns").append(li);
-			};
-			
-
-			// Calls youtube function to generate info for link in 
-			getYouTube();
-							
-	});
-});
-
-// This section handles the youtube video data
-function getYouTube(response2){
-	// response2.preventDefault();
-	
-	var id = localStorage.getItem("gameID");
-	var queryYouTube = `https://api.rawg.io/api/games/${id}/youtube`;
-	
-	
-	$.ajax({
-		url: queryYouTube,
-		method: "GET"
-	}).then(function(response2){
-		console.log(response2);
-		// console.log("id:", id);
-		// console.log(externalID);
-
-		var externalID = response2.results[0].external_id;
-		$("#game-title").attr("href", "https://www.youtube.com/watch?v=" + externalID);
-	});
-};
-
-
-// THis section handles the achievement data
-// $(".button").on("click", function(response3){
-// 	console.log(response3);
-
-// 	var id2 = localStorage.getItem("gameID");
-// 	var queryAchievements = `https://api.rawg.io/api/games/${id2}/achievements`;
-
-// 	$.ajax({
-// 		url:queryAchievements,
-// 		method: "GET"
-// 	}).then(function(response3){
-// 		console.log(response3);
-
-// 		// $("#gameReturns").children.
-// 	})
-// });
 	
 ///////////////////////// MODAL CODE BELOW //////////////////////////////////
 
@@ -194,3 +188,19 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+///////////////////////// Display last search //////////////////////////////////
+
+//Get Local Storage
+window.onload = function() {
+	gifSlideShow();
+	displayGameInfo();
+	}
+
+///////////////////////// Clear Local Storage ///////////////////////////////////	
+
+$("#resetBtn").click( function resetPage(e){
+	e.preventDefault();
+	localStorage.clear('game');
+	location.reload();
+});
